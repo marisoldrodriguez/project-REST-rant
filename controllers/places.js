189b8bab37801a20d0e9a2f4f8bc8e7e25df5a0f
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-11 16:03:12 -0400

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -27,7 +27,10 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-
+// CREATE
+router.post('/', (req, res) => {
+    res.send('POST /places')
+})
 
 // exports routes from this file (Module exports are the instruction that tells Node. js which bits of code (functions, objects, strings, etc.) to “export” from a given file so other files are allowed to access the exported code.)
 module.exports = router
