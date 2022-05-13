46d8e3fed9116105bb5619e9d162a711ded83c0c
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-13 16:41:11 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -13,10 +13,18 @@ router.get("/", (req, res) => {
 
 // CREATE
 router.post("/", (req, res) => {
-  console.log(req.body)
-  // console.log(req)
-
-  res.send("POST /places")
+  if(!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if(!req.body.city){
+    req.body.city ='Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state ='USA'
+  }
+  places.push(req.body)
+  res.redirect("/places")
 });
 
 // route for NEW view
