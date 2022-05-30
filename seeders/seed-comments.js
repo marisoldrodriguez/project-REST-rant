97a49986efae6bf81e9e6beb37bfe8893b8bb1b2
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-30 16:19:06 -0400

    Successful comment seed data.

---
+++ seeders/seed-comments.js
@@ -0,0 +1,36 @@
+const db = require('../models')
+
+// To use await, we need an async function.
+async function seed() {
+    // Get the place, H-Thai-ML
+    let place = await db.Place.findOne({ name: 'H-Thai-ML' })
+
+    // Create a fake sample comment.
+    let comment = await db.Comment.create([{
+        author: 'Famished Fran',
+        rant: false,
+        stars: 5.0,
+        content: 'Wow, simply amazing! Highly recommended!'
+    }, {
+        author: 'Stumbling Stan',
+        rant: false,
+        stars: 5.0,
+        content: 'Great food!'
+    }])
+
+    // Add that comment to the place's comment array.
+    place.comments.push(comment.id)
+
+    //save the place now that it has comment
+    await place.save()
+    
+    .then(() => {
+        console.log('Success!')
+    })
+    .catch(err => {
+        console.log('Failure!', err)
+        process.exit()
+    })
+}
+
+seed()
