cce421f550517c3e03a1119a7ae1b29a047b9aa3
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-31 13:23:28 -0400

    Calculated and displayed star ratings on the show page.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,3 +1,4 @@
+const text = require('body-parser/lib/types/text')
 const React = require('react')
 const Def = require('../default')
 
@@ -5,7 +6,21 @@ function show (data) {
     let comments = (
         <h3 className='inactive'>No comments yet!</h3>
     )
+    let rating = (
+        <h3 className='inactive'>Not yet rated.</h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce ((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
+        rating = (
+            <h3>{stars} stars</h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className='border'>
@@ -31,7 +46,8 @@ function show (data) {
                     <div className='col-sm-6'>
                     <h1>{data.place.name}</h1>     
                             <h2>Rating</h2>
-                            <p>Not Rated</p>
+                            {rating}
+                            <br/>
                             <h2>Description</h2>
                             <h3>
                                 {data.place.showEstablished()}
