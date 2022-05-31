9dffaff7b2886a519b396587b9530d4b34cfa1fd
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-31 13:47:27 -0400

    PUT and DELETE functional with MongoDB.

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form action={`/places/${data.id}?_method=PUT`}  method="POST">
+                <form action={`/places/${data.place.id}?_method=PUT`}  method="POST">
 				<div className='row'>
                     <div className='form-group col-sm-6'>
                         <label htmlFor='name'>Place Name</label> 
@@ -28,11 +28,16 @@ function edit_form (data) {
                         <input className='form-control' type="text" id="state" name="state" value={data.place.state} />
  					</div>
 				</div>
-                    <div className='form-group'>
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className='form-control' type="text" id="cuisines" name="cuisines" value={data.place.cuisines} required />
-                    </div>
-                    
+				<div className='row'>
+						<div className='form-group col-sm-6'>
+							<label htmlFor="cuisines">Cuisines</label>
+							<input className='form-control' type="text" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+						</div>
+						<div className='form-group col-sm-6'>
+							<label htmlFor="founded">Founded</label>
+							<input className='form-control' type="text" id="founded" name="founded" value={data.place.founded} required />
+						</div>
+				</div>     
                     <input className='btn btn-primary' type='submit' value='Edit Place' />
                 </form>
             </main>
