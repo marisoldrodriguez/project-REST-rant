7e7daf1c5612785375b8981c2f282477a660888c
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-06-01 11:19:06 -0400

    changed  to '' in controller/places.js file

--- controllers/places.js
+++ controllers/places.js
@@ -3,128 +3,127 @@
 // const res = require("express/lib/response");
 
 // The express.Router() function is used to create a new router object. This function is used when you want to create a new router object in your program to handle requests.
-const router = require('express').Router()
-const db = require('../models')
+const router = require("express").Router();
+const db = require("../models");
 
 // INDEX
-router.get('/', (req, res) => {
-    db.Place.find()
+router.get("/", (req, res) => {
+  db.Place.find()
     .then((places) => {
-      res.render('places/index', { places })
+      res.render("places/index", { places });
     })
-    .catch(err => {
-      console.log(err)
-      res.render('error404')
-    })
-})
-
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
+});
 
 // CREATE
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
 // NEW
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
 // SHOW
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
-  .populate('comments')
-  .then(place => {
-    console.log(place.comments)
-      res.render('places/show', { place })
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
-})
-
+    .populate("comments")
+    .then((place) => {
+      console.log(place.comments);
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
 // EDIT
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   db.Place.findByIdAndUpdate(req.params.id, req.body)
-  .then(() => {
-    res.redirect(`/places/${req.params.id}`)
-  })
-  .catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
-})
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
 // DELETE
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   db.Place.findByIdAndDelete(req.params.id)
-  .then(place => {
-    res.redirect('/places')
-  })
-  .catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
-})
+    .then((place) => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
 // EDIT form
-router.get('/:id/edit', (req, res) => {
+router.get("/:id/edit", (req, res) => {
   db.Place.findById(req.params.id)
-  .then(place => {
-    res.render('places/edit', { place })
-  })
-  .catch(err => {
-    res.render('error404')
-  })
-})
+    .then((place) => {
+      res.render("places/edit", { place });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
 // Rants (Comments)
-router.post('/:id/comment', (req, res) => {
-  console.log('post comment', req.body)
-  if (req.body.author === '') { req.body.author = undefined }
-    req.body.rant = req.body.rant ? true : false
-    db.Place.findById(req.params.id)
-        .then(places => {
-            db.Comment.create(req.body)
-                .then(comment => {
-                    places.comments.push(comment.id)
-                    places.save()
-                        .then(() => {
-                            res.redirect(`/places/${req.params.id}`)
-                        })
-                        .catch(err => {
-                            res.render('error404')
-                        })
-                })
-                .catch(err => {
-                    res.render('error404')
-                })
+router.post("/:id/comment", (req, res) => {
+  console.log("post comment", req.body);
+  if (req.body.author === "") {
+    req.body.author = undefined;
+  }
+  req.body.rant = req.body.rant ? true : false;
+  db.Place.findById(req.params.id)
+    .then((places) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          places.comments.push(comment.id);
+          places
+            .save()
+            .then(() => {
+              res.redirect(`/places/${req.params.id}`);
+            })
+            .catch((err) => {
+              res.render("error404");
+            });
         })
-        .catch(err => {
-            res.render('error404')
-        })
-})
+        .catch((err) => {
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+});
 
-router.delete('/:id/comment/:commentId', (req, res) => {
+router.delete("/:id/comment/:commentId", (req, res) => {
   db.Comment.findByIdAndDelete(req.params.commentId)
-      .then(() => {
-          console.log('Success')
-          res.redirect(`/places/${req.params.id}`)
-      })
-      .catch(err => {
-          console.log('err', err)
-          res.render('error404')
-      })
-})
+    .then(() => {
+      console.log("Success");
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
+});
 
-module.exports = router
-
+module.exports = router;
